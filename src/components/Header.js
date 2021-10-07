457a4e1fd6a8802a3e1a848025a3a029b0735e5d
Javier Valladares <javiervall@gmail.com>
2021-10-07 17:01:31 -0300

    Se realiza instalacion y limpieza del boilerplate, se crea carpeta components y se crea archivo NavBar.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h1>
+        <span>SVJ</span> Store
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function NavBar() {
+  return (
+    <div className="nav-container">
+      <nav className="nav-bar">
+        <ul className="menu-nav">
+          <li>
+            <a href="./index.html">Inicio</a>
+          </li>
+          <li>
+            <a href="#about">Quienes Somos</a>
+          </li>
+          <li>
+            <a href="#categorias">Categorias</a>
+            <ul className="submenu">
+              <li>Equipamiento Deportivo</li>
+              <li>Maquinas y Home Gym</li>
+              <li>Suplementos</li>
+            </ul>
+          </li>
+          <li>
+            <a href="#contacto">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
